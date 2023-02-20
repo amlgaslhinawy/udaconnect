b10c614b31928ddf608f48d73cc6b36b9da7c3fc
amlgalshinawy <amlgalshinawy@gmail.com>
2023-02-20 21:30:47 +0200

    connection deployment

--- modules/2-connection_microservice/app/udaconnect/services.py
+++ modules/2-connection_microservice/app/udaconnect/services.py
@@ -2,11 +2,15 @@ import logging
 from datetime import datetime, timedelta
 from typing import Dict, List
 
-from app import db
-from app.udaconnect.models import Connection, Location, Person
-from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
-from geoalchemy2.functions import ST_AsText, ST_Point
+import requests
+from geoalchemy2.functions import ST_Point
 from sqlalchemy.sql import text
+
+from app import db
+from app.config import PERSON_SERVICE_ENDPOINT
+from app.udaconnect.models import Connection, Location, Person
+from app.udaconnect.schemas import LocationSchema
+
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("udaconnect-api")
